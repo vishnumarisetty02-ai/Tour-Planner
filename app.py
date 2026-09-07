3e88702e30ff75d196eb6122dae7bd3b09d24a51
M.venkata vishnu vardhan <vishnumarisetty02@gmail.com>
2026-09-07 18:45:51 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ st.set_page_config(
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
+if not API_KEY:
+    API_KEY = st.secrets.get("GROQ_API_KEY")
 if not GROQ_API_KEY:
     st.error(
         "GROQ_API_KEY not found.\n\n"
